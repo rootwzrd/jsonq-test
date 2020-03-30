c76c7158a56f7c6a35a0df74467b353df244573e
Chris <rootwzrd@gmail.com>
2020-03-29 22:18:32 -0400

    git add and initial commit pushed up

---
+++ jtest.js
@@ -0,0 +1,17 @@
+var jsonQ = require("jsonq");
+var jdata = require("./Beauty-feed-filtered-base");
+var fdata = require("./Fnames");
+
+var jkey = "Additional Product Picture 4";
+
+//console.log(jdata[7][jkey])
+
+console.log("Finished")
+
+ 
+
+//console.log(Object.keys(jdata[7]));
+//console.log(Object.values(jdata[7]));
+
+var lineKeyData = Object.keys(jdata[8]);
+console.log(lineKeyData);
